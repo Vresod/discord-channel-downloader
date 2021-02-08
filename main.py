5d8182eacd0c390e3c8bee80cb64e53ac89e39ee
Vresod <lukebinkofsky@gmail.com>
2021-02-08 10:39:45 -0500

    bot complete

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 import discord
-import requests
 from sys import argv
 
-channel_id = argv[1]
+channel_id = int(argv[1])
 path = "." if len(argv) < 3 else argv[2]
-print(channel_id,path)
 
 client = discord.Client()
 
+disallowed_chars = ["\\","/",":","*","?","<",">","|"," "]
+
+def fix_name(name,replace = "-"):
+	for i in ["\\","/",":","*","?","<",">","|"," "]:
+		name = name.replace(i,replace)
+	return name
+
 with open("tokenfile", "r") as tokenfile:
-	token=tokenfile.read()
+	token = tokenfile.read()
+
+@client.event
+async def on_ready():
+	print(f"logged in as {client.user}")
+	print(f"https://discord.com/oauth2/authorize?client_id={client.user.id}&permissions=0&scope=bot")
 
 @client.event
 async def on_message(message):
-	if message.channel.id != channel_id:
+	if message.channel.id != channel_id or len(message.attachments) == 0:
 		return
-	
-	for x in message.attachments:
-		r = requests.get(x.url)
-		with open(f"{path}/{x.filename}-{message.author.name}.png", 'wb') as file:
-			for chunk in r:
-				file.write(chunk)
+	authorname = message.author.display_name
+	for char in disallowed_chars:
+		authorname = authorname.replace(char,"-")
+	if message.content == "":
+		filename = f"{path}/{fix_name(message.author.display_name)}.png"
+	else:
+		filename = f"{path}/{fix_name(message.author.display_name)}-{fix_name(message.content)}.png"
+	for attachment in message.attachments:
+		await attachment.save(filename)
+
+client.run(token)
